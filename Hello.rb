17c8a416228fa8b7ae52809cec1a1c444bdd0b2f
fernandodxx <fernandosillvasantoss@gmail.com>
2024-05-23 11:40:24 -0300

    Moving Things With Keyboard

--- Hello.rb
+++ Hello.rb
@@ -3,13 +3,43 @@ require 'gosu'
 class GameWindow < Gosu::Window
   def initialize(width=500, height=350, fullscreen=false)
     super
-    self.caption = 'Hello'
-    @message = Gosu::Image.from_text(
-      self, 'Hello, World!', Gosu.default_font_name, 30)
+      self.caption = 'Hello Movement'
+      @x = @y = 10
+      @draws = 0
+      @buttons_down = 0
+  end
+
+  def update
+    @x -= 1 if button_down?(Gosu::KbLeft)
+    @x += 1 if button_down?(Gosu::KbRight)
+    @y -= 1 if button_down?(Gosu::KbUp)
+    @y += 1 if button_down?(Gosu::KbDown)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
+    @buttons_down += 1
   end
  
+  def button_up(id)
+    @buttons_down -= 1
+  end
+ 
+  def needs_redraw?
+    @draws == 0 || @buttons_down > 0
+  end
+
   def draw
-    @message.draw(10, 10, 0)
+    @draws += 1
+    @message = Gosu::Image.from_text(
+      self, info, Gosu.default_font_name, 30)
+    @message.draw(@x, @y, 0)
+  end
+
+  private
+
+  def info
+    "[x:#{@x};y:#{@y};draws:#{@draws}]"
   end
 end
  
